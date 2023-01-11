0addf2ab09619115a0f82d3ff04dc7df513101a1
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-11 17:18:31 +0200

    Create echoFunction.js

---
+++ 1. Syntax, Functions and Statements - Lab/echoFunction.js
@@ -0,0 +1,13 @@
+// Write a JS function that takes one string parameter 
+// and prints on two lines the length of the parameter and then the unchanged parameter itself.
+
+function echoFunction(inputString) {
+
+    console.log(inputString.length);
+    console.log(inputString);
+    
+}
+
+echoFunction('Hello, JavaScript!')
+
+echoFunction('strings are easy')
