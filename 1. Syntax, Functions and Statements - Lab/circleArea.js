42c1784bb67eb35d955cda8f5e8bdce21eceaa21
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-11 17:19:07 +0200

    Create circleArea.js

---
+++ 1. Syntax, Functions and Statements - Lab/circleArea.js
@@ -0,0 +1,24 @@
+// Write a function that takes a single argument as an input. Check the type of input argument. If it is a number, assume it is the radius of a circle and calculate the circle area. Print the area rounded to two decimal places.
+// If the argument type is NOT a number, print the following text on the console: 
+// `We can not calculate the circle area, because we receive a {type of argument}.`
+// The input comes as a single argument passed to your function.
+// The output should be printed on the console.
+
+function circleArea(input) {
+
+    if (typeof input === 'number') {
+
+        let area = Math.PI * (input ** 2);
+
+        console.log(area.toFixed(2));
+
+    } else {
+
+        console.log(`We can not calculate the circle area, because we receive a ${typeof input}.`);
+    }
+
+}
+
+circleArea(5)
+
+circleArea('name')
