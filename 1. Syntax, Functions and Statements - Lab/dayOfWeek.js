2340ebaa0cd1fc59615752ce54a1e701aded344c
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-12 22:12:20 +0200

    Create dayOfWeek.js

---
+++ 1. Syntax, Functions and Statements - Lab/dayOfWeek.js
@@ -0,0 +1,27 @@
+// Write a function that prints a number between 1 and 7 when a day of the week is passed to it as a string and an error message if the string is not recognized.
+// The input comes as a single-string argument.
+// The output should be returned as a result.
+
+function dayOfWeek(day) {
+
+    let output;
+
+    switch (day) {
+        case "Monday": output = 1; break;
+        case "Tuesday": output = 2; break;
+        case "Wednesday": output = 3; break;
+        case "Thursday": output = 4; break;
+        case "Friday": output = 5; break;
+        case "Saturday": output = 6; break;
+        case "Sunday": output = 7; break;
+        default: output = "error"; break;
+    }
+
+    console.log(output);
+}
+
+dayOfWeek('Monday')
+
+dayOfWeek('Friday')
+
+dayOfWeek('Invalid')
