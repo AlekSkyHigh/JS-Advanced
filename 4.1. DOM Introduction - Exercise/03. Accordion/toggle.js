7adc4ff359cc939147e5cd2a6fbda4241a93a803
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-01-28 10:38:18 +0200

    Create toggle.js

---
+++ 4.1. DOM Introduction - Exercise/03. Accordion/toggle.js
@@ -0,0 +1,16 @@
+function toggle() {
+    
+    let button = document.querySelector('.button');
+    let text = document.querySelector('#extra');
+
+    if(button.textContent === 'More'){
+        button.textContent = 'Less';
+        text.style.display = 'block';
+
+    } else if(button.textContent === 'Less') {
+        button.textContent = 'More';
+        text.style.display = 'none';
+    }
+    
+};
+
