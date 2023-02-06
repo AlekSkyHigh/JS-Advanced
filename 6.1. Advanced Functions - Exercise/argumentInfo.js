3d38de383db55703265ca197394b1d31e4e7d5c6
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-02-06 18:17:17 +0200

    Create argumentInfo.js

---
+++ 6.1. Advanced Functions - Exercise/argumentInfo.js
@@ -0,0 +1,21 @@
+function argumentInfo() {
+    // console.log(arguments);
+    
+    let data = {};
+
+    Array.from(arguments).forEach((line) => {
+        let type = typeof line;
+        console.log(`${type}: ${line}`);
+
+        if (!data[type]) {
+            data[type] = 0;
+        }
+        data[type]++
+    });
+
+    Object.keys(data)
+        .sort((a, b) => data[b] - data[a])
+        .forEach((key) => console.log(`${key} = ${data[key]}`));
+}
+
+argumentInfo('cat', 42, function () { console.log('Hello world!'); })
